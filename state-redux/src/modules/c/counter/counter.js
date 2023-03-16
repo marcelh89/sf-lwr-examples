47447e4803bc4d7293e771be26b01c6ce6bb0c8f
Marcel Hinderlich <m.hinderlich@reply.de>
2023-03-16 21:16:44 +0100

    replaced deprecated createStore

--- state-redux/src/modules/c/counter/counter.js
+++ state-redux/src/modules/c/counter/counter.js
@@ -1,29 +1,30 @@
-import { LightningElement, api } from 'lwc';
-import { createStore } from 'redux';
-import { counterReducer } from './reducers';
+import { LightningElement, api } from "lwc";
+import { configureStore } from "@reduxjs/toolkit";
+
+import { counterReducer } from "./reducers";
 
 export default class Counter extends LightningElement {
-    @api store;
-    count = 0;
-    unsubscribe;
+  @api store;
+  count = 0;
+  unsubscribe;
 
-    connectedCallback() {
-        this.store = createStore(counterReducer);
-        this.unsubscribe = this.store.subscribe(() => {
-            const state = this.store.getState();
-            this.count = state.count;
-        });
-    }
+  connectedCallback() {
+    this.store = configureStore({ reducer: counterReducer });
+    this.unsubscribe = this.store.subscribe(() => {
+      const state = this.store.getState();
+      this.count = state.count;
+    });
+  }
 
-    disconnectedCallback() {
-        this.unsubscribe();
-    }
+  disconnectedCallback() {
+    this.unsubscribe();
+  }
 
-    handleIncrement() {
-        this.store.dispatch({ type: 'INCREMENT' });
-    }
+  handleIncrement() {
+    this.store.dispatch({ type: "INCREMENT" });
+  }
 
-    handleDecrement() {
-        this.store.dispatch({ type: 'DECREMENT' });
-    }
+  handleDecrement() {
+    this.store.dispatch({ type: "DECREMENT" });
+  }
 }
